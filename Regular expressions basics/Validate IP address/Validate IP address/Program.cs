e2afae4f83c02f154015e15f6dfe8c89e2315dac
Ani Mehrabyan <ani.mehrabyan7@gmail.com>
2013-05-22 02:08:53 +0500

    Regular expressions basics
    Regular expressions basics

--- Regular expressions basics/Validate IP address/Validate IP address/Program.cs
+++ Regular expressions basics/Validate IP address/Validate IP address/Program.cs
@@ -12,18 +12,27 @@ namespace Validate_IP_address
         static void Main(string[] args)
         {
             string ipaddress;
-            Console.Write("Input your IP Address,please   ");
+            int position;
+            int number1 = -1, number2 = -1, number3 = -1, number4 = -1;
+            Console.Write("Please, enter your IP Address    ");
             ipaddress = Console.ReadLine();
-            try
+            Regex rgs = new Regex(@"^(\{{0,1}((([0-9]){3})||([0-9]{2})||([0-9]{1})).((([0-9]){3})||(([0-9]){2})||(([0-9]){1})).((([0-9]){3})||(([0-9]){2})||(([0-9]){1})).((([0-9]){3})||(([0-9]){2})||(([0-9]){1}))\}{0,1})$");
+            if (rgs.IsMatch(ipaddress))
             {
-                IPAddress address = IPAddress.Parse(ipaddress);
-                // Display the address in standard notation.
-                Console.WriteLine("Thank you, for inputting your IP Address   " + ipaddress);
+                position = ipaddress.IndexOf('.');
+                number1 = Int32.Parse(ipaddress.Substring(0, position));
+                ipaddress = ipaddress.Remove(0, position + 1);
+                position = ipaddress.IndexOf('.');
+                number2 = Int32.Parse(ipaddress.Substring(0, position));
+                ipaddress = ipaddress.Remove(0, position + 1);
+                position = ipaddress.IndexOf('.');
+                number3 = Int32.Parse(ipaddress.Substring(0, position));
+                ipaddress = ipaddress.Remove(0, position + 1);
+                number4 = Int32.Parse(ipaddress);
             }
-            catch 
-            {
-                Console.WriteLine("{0} this string can not be IP addres",ipaddress);
-            }
+            if (number1 > -1 && number1 < 256 && number2 > -1 && number2 < 256 && number3 > -1 && number3 < 256 && number4 > -1 && number4 < 256)
+                Console.WriteLine("Thank you");
+            else Console.WriteLine("It's not IP address, Please try again.");
         }
     }
 }
