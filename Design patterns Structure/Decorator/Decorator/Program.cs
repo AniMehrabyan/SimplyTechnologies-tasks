eb7f9bd671770735d7b0abc31593423153d9e4ee
Ani Mehrabyan <ani.mehrabyan7@gmail.com>
2013-06-30 17:13:21 +0500

    Facade and Decorator
    Facade and Decorator

---
+++ Design patterns Structure/Decorator/Decorator/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public interface IComponent
+    {
+        void AddTopping();
+    }
+
+    public class PlainIceCream : IComponent
+    {
+        void IComponent.AddTopping()
+        {
+            Console.WriteLine("You can add topping to your ice-cream");
+        }
+    }
+
+    public abstract class Topping : IComponent
+    {
+        protected IComponent input;
+
+        public Topping(IComponent type)
+        {
+            input = type;
+        }
+
+        void IComponent.AddTopping()
+        {
+        }
+    }
+
+    public class CaramelTopping : Topping, IComponent
+    {
+        public CaramelTopping(IComponent type)
+            : base(type)
+        {
+        }
+
+        void IComponent.AddTopping()
+        {
+            input.AddTopping();
+            Console.WriteLine("Caramel   Topping added");
+        }
+    }
+
+    public class ChocolateTopping : Topping, IComponent
+    {
+        public ChocolateTopping(IComponent type)
+            : base(type)
+        {
+        }
+
+        void IComponent.AddTopping()
+        {
+            input.AddTopping();
+            Console.WriteLine("Chocolate Topping added");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IComponent PlainIceCream = new PlainIceCream();
+            IComponent CaramelTopping = new CaramelTopping(PlainIceCream);
+            IComponent ChocolateTopping = new ChocolateTopping(CaramelTopping);
+            ChocolateTopping.AddTopping();
+        }
+    }
+}
---
+++ Design patterns Structure/Facade/Facade/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    class Program
+    {
+        internal class Book
+        {
+            internal string FindBook()
+            {
+                return "Find a book for  borrowing";
+            }
+            internal string Returned()
+            {
+                return "The book is returned";
+            }
+        }
+        internal class Person
+        {
+            internal string Return()
+            {
+                return "Person returns a book. ";
+            }
+            internal string Borrow()
+            {
+                return "Person borrows a book. ";
+            }
+        }
+        public static class Facade
+        {
+            static Book book = new Book();
+            static Person person = new Person();
+            public static void ReturnBook()
+            {
+                Console.WriteLine(person.Return());
+                Console.WriteLine(book.Returned());
+            }
+
+            public static void BorrowBook()
+            {
+                Console.WriteLine(book.FindBook());
+                Console.WriteLine(person.Borrow());
+            }
+        }
+        static void Main()
+        {
+            Facade.BorrowBook();
+            Facade.ReturnBook();
+        }
+    }
+}
