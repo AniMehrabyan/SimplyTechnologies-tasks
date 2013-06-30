f2c65445df53f41f2b4276e9c728a956f7da87d9
Ani Mehrabyan <ani.mehrabyan7@gmail.com>
2013-07-01 01:01:53 +0500

    State
    State

---
+++ Design patterns Behavioral/State/State/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    class Program
+    {
+        abstract class SkypeState
+        {
+            public abstract void Handle(Context context);
+        }
+
+        class Online : SkypeState
+        {
+            public override void Handle(Context context)
+            {
+                context.State = new DoNotDisturb();
+            }
+        }
+        class DoNotDisturb : SkypeState
+        {
+            public override void Handle(Context context)
+            {
+                context.State = new Offline();
+            }
+        }
+        class Offline : SkypeState
+        {
+            public override void Handle(Context context)
+            {
+                context.State = new Online();
+            }
+        }
+
+        class Context
+        {
+            private SkypeState state;
+            public Context(SkypeState state)
+            {
+                this.State = state;
+            }
+            public SkypeState State
+            {
+                get { return state; }
+                set
+                {
+                    state = value;
+                    Console.WriteLine("SkipeState is an " +
+                      state.GetType().Name);
+                }
+            }
+
+            public void ShowSKypeState()
+            {
+                state.Handle(this);
+            }
+        }
+        static void Main()
+        {
+            Context context = new Context(new Online());
+            context.ShowSKypeState();
+            context.ShowSKypeState();
+            context.ShowSKypeState();
+            context.ShowSKypeState();
+        }
+    }
+}
