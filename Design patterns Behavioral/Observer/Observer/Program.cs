4f2b78be2d660cdb2c77f48bf6f6bd9a308b4973
Ani Mehrabyan <ani.mehrabyan7@gmail.com>
2013-07-01 03:18:11 +0500

    Observer
    Observer

---
+++ Design patterns Behavioral/Observer/Observer/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class Subject
+    {
+        private List<Employee> employees = new List<Employee>();
+
+        public void Attach(Employee employee)
+        {
+            employees.Add(employee);
+        }
+
+        public void Detach(Employee employee)
+        {
+            employees.Remove(employee);
+        }
+
+        public void Notify()
+        {
+            foreach (var observer in employees)
+            {
+                observer.Update();
+            }
+        }
+    }
+    abstract class Employee
+    {
+        abstract public void Update();
+    }
+    class ConcreteSubject : Subject
+    {
+        private int value;
+
+        public int Value
+        {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                this.value = value;
+                Notify();
+            }
+        }
+    }
+    class ConcreteObserver : Employee
+    {
+        private ConcreteSubject subject;
+
+        public override void Update()
+        {
+            Console.WriteLine("Subject value is {0}.", subject.Value);
+        }
+
+        public ConcreteObserver(ConcreteSubject subject)
+        {
+            subject.Attach(this);
+            this.subject = subject;
+        }
+
+        ~ConcreteObserver()
+        {
+            subject.Detach(this);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteSubject concreteSubject = new ConcreteSubject();
+            ConcreteObserver concreteObserver = new ConcreteObserver(concreteSubject);
+
+            concreteSubject.Value = 10;
+        }
+    }
+}
